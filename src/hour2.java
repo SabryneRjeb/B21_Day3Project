e5847f80ab5890fa49eb6a7030dc83a5f0a6994f
SabryneRjeb <sabryne.rjeb@gmail.com>
2021-01-03 14:40:15 -0500

    branch hour2 commit

---
+++ src/hour2.java
@@ -0,0 +1,7 @@
+public class hour2 {
+    public static void main(String[] args) {
+        System.out.println("hour 2 branch");
+        // branching and more branching
+        // practicing more branching
+    }
+}
